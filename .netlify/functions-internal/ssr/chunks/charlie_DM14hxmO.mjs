94eee1696161448e13cc5527205385b08f5c109a
Xjwmx <70461586+Xjwmx@users.noreply.github.com>
2024-05-19 13:26:15 -0700

    chore: Remove unused files and update styles

--- .netlify/functions-internal/ssr/chunks/charlie_DM14hxmO.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/charlie_CsAHWnIK.mjs');
-
-export { page };
---
+++ src/assets/js/fullscreen.js
@@ -0,0 +1,11 @@
+function openFullscreen(element) {
+	if (element.requestFullscreen) {
+	  element.requestFullscreen();
+	} else if (element.mozRequestFullScreen) { /* Firefox */
+	  element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+	  element.webkitRequestFullscreen();
+	} else if (element.msRequestFullscreen) { /* IE/Edge */
+	  element.msRequestFullscreen();
+	}
+  }
